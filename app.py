df3eb70b3c18147c0aa23ddcf269236d30d09524
Aleix <aleixurbanoroura@gmail.com>
2024-10-29 14:41:02 +0100

    Added initialize function to store the products in case they don't exist.

--- app.py
+++ app.py
@@ -1,29 +1,45 @@
 from flask import Flask, render_template, url_for, request, redirect, flash
-from classes.Order import Order
-from classes.Product import Product
+
+from classes.DataManager import DataManager
 from classes.OrderLine import OrderLine
+from classes.Product import Product
 
 app = Flask(__name__)
-app.secret_key = 'secret_key' 
+app.secret_key = 'secret_key'
 
-pizzas = [
-    Product("Pepperoni", 10.0, ['Tomatoes, Garlic, Herbs, Mozzarella, Pepperoni'], ['gluten', 'milk'], 'images/pepperoni.jpeg'),
-    Product("Margherita", 10.0, ['Tomatoes, Mozzarella, Fresh basil, Olive oil'], ['gluten', 'milk'], 'images/Margherita.jpeg'),
-    Product("Neapolitan", 10.0, ['San Marzano tomatoes, Mozzarella di bufala, Fresh basil, Olive oil'], ['gluten', 'milk'], 'images/Neapolitan.jpeg'),
-    Product("Romana", 10.0, ['Tomatoes, Mozzarella, Oregano, Anchovies (or other toppings)'], ['gluten', 'milk'], 'images/Romana.jpg')
-]
+dataManager = DataManager()
+
+
+def initialize():
+    """
+    This method will initialize the application.
+    """
+    if dataManager.products.count() < 1:
+        dataManager.products = [
+            Product("Pepperoni", 10.0, ['Tomatoes, Garlic, Herbs, Mozzarella, Pepperoni'], ['gluten', 'milk'],
+                    'images/pepperoni.jpeg'),
+            Product("Margherita", 10.0, ['Tomatoes, Mozzarella, Fresh basil, Olive oil'], ['gluten', 'milk'],
+                    'images/Margherita.jpeg'),
+            Product("Neapolitan", 10.0, ['San Marzano tomatoes, Mozzarella di bufala, Fresh basil, Olive oil'],
+                    ['gluten', 'milk'], 'images/Neapolitan.jpeg'),
+            Product("Romana", 10.0, ['Tomatoes, Mozzarella, Oregano, Anchovies (or other toppings)'], ['gluten', 'milk'],
+                    'images/Romana.jpg')
+        ]
+        dataManager.saveProducts()
 
 fohOrderLineList = []
+
 
 @app.route('/')
 def index():
     return render_template('index.html', pizzas=pizzas)
 
+
 @app.route('/order', methods=['POST'])
 def order():
     pizza_name = request.form.get('pizza_name')
     selected_pizza = next((pizza for pizza in pizzas if pizza.name == pizza_name), None)
-    
+
     if selected_pizza:
         pizza_price = selected_pizza.price
         pizza_image = selected_pizza.images
@@ -31,12 +47,12 @@ def order():
     else:
         flash("Pizza not found!")
         return redirect(url_for('index'))
-    
+
+
 @app.route("/foh-create-order", methods=['POST', 'GET'])
 def fohOrder():
-
     addedPizzaName = request.form.get("addedPizza")
-    
+
     try:
         addedPizzaQuantity = int(request.form.get("addedQuantity"))
     except ValueError as a:
@@ -44,7 +60,7 @@ def fohOrder():
 
     print(addedPizzaName)
     print(addedPizzaQuantity)
-    
+
     selectedPizza = next((pizza for pizza in pizzas if pizza.name == addedPizzaName), None)
 
     if selectedPizza:
@@ -61,8 +77,8 @@ def fohOrder():
                 fohOrderLineList.append(createdOrderline)
 
     # currently holding place holder values
-    return render_template('fohOrderPage.html', completionCount = 4, tableNumber = 12, filteredProducts = pizzas, orderList = fohOrderLineList)
-
+    return render_template('fohOrderPage.html', completionCount=4, tableNumber=12, filteredProducts=pizzas,
+                           orderList=fohOrderLineList)
 
 
 @app.route('/modify', methods=['POST'])
@@ -71,9 +87,12 @@ def modify():
     flash(f"You are modifying the {pizza_name} pizza!")
     return redirect(url_for('index'))
 
+
 @app.routre('/fohOverview')
 def fohOverview():
     return render_template('overveiw.html')
 
+
 if __name__ == '__main__':
+    initialize()
     app.run(debug=True)
